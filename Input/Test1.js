3b70ef0a7dc34186b18fbc8dd56a179648c1f89c
Mika du Toit <878832@students.wits.ac.za>
2020-05-15 01:05:59 +0200

    Update Test1.js

--- Input/Test1.js
+++ Input/Test1.js
@@ -6,6 +6,6 @@
     public class Hello {
         public static final void main(String [] args) { // gotta love Java
             // Say hello
-          System./*wait*/out./*for*/println/*it*/("Hello/*");
+          System./*wait*/out./*for*/println/*it*/('Hello/*');
         }
     }
